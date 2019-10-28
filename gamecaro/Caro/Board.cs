cc6cd64451876434d9c2e6448b3dbf593bc0959a
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-10-29 01:22:15 +0700

    gamecaro

---
+++ gamecaro/Caro/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caro
+{
+    class Board
+    {
+    private char[,] Arr;
+    private string P1, P2;
+    public Board()
+    {
+        this.Arr = new char[20, 20];
+        this.P1 = new string() {"Player 1"};
+        this.P2 = new string() {"Player 2"};
+    }
+
+    }
+}
---
+++ gamecaro/Caro/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caro
+{
+    class Player
+    {
+        public Player(string name)
+        {
+            Console.Write("Row: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write
+        }
+    }
+}
